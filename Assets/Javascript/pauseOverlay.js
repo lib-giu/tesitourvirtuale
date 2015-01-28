6a2c3811207ac2c93579e904daebb01e34ccb2d4
Simone Basso <bassosimone@gmail.com>
2015-01-28 16:58:19 +0100

    Rename Pause.js to pauseOverlay.js
    As usual add also come comments and reindent a bit

--- Assets/Javascript/pauseOverlay.js
+++ Assets/Javascript/pauseOverlay.js
@@ -1,28 +1,30 @@
 ﻿#pragma strict
 
+//
+// The overlay that appears when you press Esc
+//
+
 var pauseGame : boolean = false;
 var canvasMenu : Canvas;
 var canvasInfoBook : Canvas;
 
 function Start () {
-
+	/* nothing to do */
 }
 
 function Update () {
+	if (canvasInfoBook.enabled == false){
+		if (Input.GetKeyDown("escape")){
 
-	if(canvasInfoBook.enabled == false){
-		if(Input.GetKeyDown("escape")){
-		
 			pauseGame = !pauseGame;
-		
-			if(pauseGame == true){
-			
+
+			if (pauseGame == true) {
 				Time.timeScale = 0;
 				GameObject.Find("Main Camera").GetComponent(MouseLook).enabled = false;
 				GameObject.Find("First Person Controller").GetComponent(MouseLook).enabled = false;
 				canvasMenu.enabled = true;
-			}else{
-			
+
+			} else {
 				Time.timeScale = 1;
 				GameObject.Find("Main Camera").GetComponent(MouseLook).enabled = true;
 				GameObject.Find("First Person Controller").GetComponent(MouseLook).enabled = true;
@@ -32,8 +34,7 @@ function Update () {
 	}
 }
 
-function Resume(){
-
+function Resume() {
 	Time.timeScale = 1;
 	GameObject.Find("Main Camera").GetComponent(MouseLook).enabled = true;
 	GameObject.Find("First Person Controller").GetComponent(MouseLook).enabled = true;
